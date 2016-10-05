d6c508b24ea9e016a5a78de777b7c73139a943c0
Darcy <darcy.ab.jones@gmail.com>
2016-10-05 20:24:13 +0800

    moving feature to a collection subclass

--- bioplotlib/collections.py
+++ bioplotlib/collections.py
@@ -16,13 +16,15 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from copy import copy
+from collections import defaultdict
 
 import numpy as np
 import matplotlib.transforms as transforms
 from matplotlib.path import Path
 from matplotlib.patches import PathPatch
+from matplotlib.patches import Patch
 
-from matplotlib.collections import PathCollection
+from matplotlib.collections import Collection
 
 import feature_shapes
 from feature_shapes import Triangle
@@ -33,103 +35,263 @@ __contributors = [
     "Darcy Jones <darcy.ab.jones@gmail.com>"
     ]
 
+def new_shape(c, **kwargs):
+     """ . """
+     def callable(*a, **k):
+         kwargs.update(k)
+         return c(*a, **kwargs)
+     return callable
+
 ################################## Classes ###################################
 
-class Feature(object):
+class Feature(Collection):
 
     """ Groups shapes into features so that they stay together
     in stacked tracks. """
 
-    valid_kwargs = [
-        "edgecolors",
-        "facecolors",
-        "linewidths",
-        "antialiaseds",
-        "offsets",
-        "transOffset",
-        "offset_position",
-        "norm",
-        "cmap",
-        "hatch",
-        "zorder"
-        ]
-
     def __init__(
             self,
+            blocks,
+            shape=new_shape(Triangle),
+            between_shape=None,
+            strand=None,
             offset=0,
-            by=None,
+            by_axis=None,
             name=None,
             **kwargs
             ):
-        """ . """
+        if type(shape) in (tuple, list):
+            self._shape = shape
+        else:
+            self._shape = [shape]
 
-        self.shapes = kwargs
-        for shape, obj in self.shapes.items():
-            if isinstance(obj, feature_shapes.Shape):
-                obj = [obj]
+        if type(between_shape) in (tuple, list, type(None)):
+            self._between_shape = between_shape
+        else:
+            self._between_shape = [between_shape]
 
-            if not (0 < len(obj) <= 3):
-                raise ValueError((
-                        "The shape {} has an invalid number of elements."
-                        "Each shape must have 1, 2, or 3 elements."
-                        ).format(shape)
-                    )
+        self._blocks = blocks
+        self._strand = strand
+        self._offset = offset
+        self._by_axis = by_axis
+        self._name = name
 
-        self.offset = offset
-        self.by = by
-        self.name = name
+        self._paths = None
+        self._patches = None
+
+        Collection.__init__(self, **kwargs)
+        self._draw_patches()
         return
 
-    def __call__(self):
-        """ . """
-        return self.draw()
+    @property
+    def blocks(self):
+        return self._blocks
 
-    def __extend_dict(self, old_dict, new_dict):
-        """ . """
-        length = 0
-        for k, v in old_dict.items():
-            if len(v) > length:
-                length = len(v)
+    @blocks.setter
+    def blocks(self, blocks):
+        self._blocks = blocks
 
-        for key, val in new_dict:
-            if key not in old_dict:
-                old_dict[key] = [None] * length
+    @property
+    def strand(self):
+        return self._strand
 
-            old_dict[key].extend(list(val))
+    @strand.setter
+    def strand(self, strand):
+        self._strand = strand
+        for patch in self.patches:
+            if patch.strand is not None:
+                patch.strand = strand
 
-        return old_dict
+    @property
+    def offset(self):
+        return self._offset
 
-    def draw(self):
-        """ . """
+    @offset.setter
+    def offset(self, offset):
+        self._offset = offset
+        if offset != 0:
+            for patch in self.patches:
+                patch.offset += offset
 
-        paths = list()
-        properties = dict()
+    @property
+    def by_axis(self):
+        return self._by_axis
 
+    @by_axis.setter
+    def by_axis(self, by_axis):
+        self._by_axis = by_axis
+        if by_axis is not None:
+            for patch in self.patches:
+                if patch.by_axis is not None:
+                    patch.by_axis = by_axis
+
+    @property
+    def shape(self):
+        return self._shape
+
+    @shape.setter
+    def shape(self, shape):
+        self._shape = shape
+        self._draw_patches()
+
+    @property
+    def between_shape(self):
+        return self._between_shape
+
+    @between_shape.setter
+    def between_shape(self, shape):
+        self._between_shape = shape
+        self._draw_patches()
+
+    @property
+    def patches(self):
+        return self._patches
+
+    @patches.setter
+    def patches(self, patches):
+        self._patches = patches
+        self._set_paths()
+        self._set_props()
+        return
+
+    @property
+    def paths(self):
+        return self._paths
+
+    @paths.setter
+    def paths(self, paths):
+        self._paths = paths
+        return
+
+    def get_paths(self):
+        """ alias for paths property """
+        return self.paths
+
+    def set_paths(self, paths):
+        """ alias for paths.setter """
+        self.paths = paths
+        return
+
+    def _set_paths(self):
+        self.paths = [
+            p.get_transform().transform_path(p.get_path())
+            for p in self.patches
+            ]
+        return
+
+    def _set_props(self):
+
+        def determine_facecolor(patch):
+            if patch.get_fill():
+                return patch.get_facecolor()
+            return [0, 0, 0, 0]
+
+        props = defaultdict(list)
+        valid = {
+            "facecolors": (self.set_facecolor, determine_facecolor),
+            "edgecolors": (self.set_edgecolor, Patch.get_edgecolor),
+            "linewidths": (self.set_linewidth, Patch.get_linewidth),
+            "linestyles": (self.set_linestyle, Patch.get_linestyle),
+            "antialiaseds": (self.set_antialiased, Patch.get_antialiased),
+            }
+
+        for p in self.patches:
+            for key, (set_, get) in valid.items():
+                props[key].append(get(p))
+
+        for key, (set_, get) in valid.items():
+            set_(props[key])
+        return
+
+    def _draw_patches(self):
         start = 0
         end = None
 
-        paths.extend([self.shapes[start].path(*p) for
-                      p in self.blocks])
+        patches = list()
 
-        shape_pos = 0
         length_blocks = len(self.blocks)
+
+        # Draw the between shapes first
+        between_shape_pos = 0
+        between_blocks = []
+        if self.between_shape is not None:
+            between_blocks = list(zip(
+                range(0, length_blocks - 1),
+                range(1, length_blocks)
+                ))
+        length_between_blocks = len(between_blocks)
+
+        # Note that if between shape is none there is nothing to iterate over.
+        for h, (i, j) in enumerate(between_blocks):
+            first = self.blocks[i]
+            second = self.blocks[j]
+            min_dist = min([(0, 0), (0, 1), (1, 0), (1, 1)],
+                key=lambda t: abs(second[t[0]] - first[t[1]])
+                )
+            start = first[min_dist[1]]
+            end = second[min_dist[0]]
+
+            strand_first = None
+            strand_second = None
+            try:
+                strand_first = first[2]
+                strand_second = second[2]
+            except:
+                pass
+
+            if strand_first == strand_second:
+                strand = strand_first
+            else:
+                strand = None
+
+            if len(self.between_shape) > 1 and h + 1 == length_between_blocks:
+                between_shape_pos = -1
+
+            patches.append(self.between_shape[between_shape_pos](
+                start=start,
+                end=end,
+                strand=None
+                ))
+
+            if len(self.between_shape) > 2:
+                between_shape_pos = 1
+
+        # Now do the blocks
+        shape_pos = 0
+
         for i, block in enumerate(self.blocks):
-            if len(self.shapes) > 1 and i + 1 == length_blocks:
+            if len(self.shape) > 1 and i + 1 == length_blocks:
                 shape_pos = -1
 
-            paths.append(self.shapes[shape_pos].path(*block))
-            properties = self.__extend_dict(
-                properties,
-                self.shapes[shape_pos].properties
-                )
+            start = block[0]
+            end = block[1]
 
-            if len(self.shapes) > 1:
+            strand = None
+            try:
+                strand = block[2]
+            except:
+                pass
+
+            patches.append(self.shape[shape_pos](
+                start=start,
+                end=end,
+                strand=strand
+                ))
+
+            if len(self.shape) > 2:
                 shape_pos = 1
 
-        return paths, properties
+        if self.offset != 0:
+            for patch in patches:
+                patch.offset += self.offset
+
+        self._patches = patches
+        self._set_paths()
+        self._set_props()
+        return
 
 
-class FeatureTrack(object):
+class FeatureGroup(Collection):
 
     """ Generic collection for genomic tracks.
 
@@ -141,72 +303,130 @@ class FeatureTrack(object):
 
     def __init__(
             self,
-            x=0,
-            y=0,
+            features,
             width=1,
+            offset=0,
+            stack=None,
+            by_axis=None,
             name=None,
-            features=list(),
-            stacking_method=None,
-            angle=0,
-            by='x',
+            **kwargs
             ):
         """ . """
-        self.x = x
-        self.y = y
-        self.width = width
-        self.features = list()
-        self.features.extend(features)
+        self._features = features
+        self._width = width
+        self._stack = stack
+        self._by_axis = by_axis
+        self._offset = offset
         self.name = name
-        self.stacking_method = stacking_method
-        self.angle = angle  # Currently doesn't do anything.
-        self.by = by
+
+        self._patches = None
+        self._paths = None
+
+
+        Collection.__init__(self, **kwargs)
+        self._draw_patches()
         return
 
-    def __call__(self):
-        return self.draw()
+    @property
+    def features(self):
+        return self._features
 
-    def draw(self, ax=None):
+    @features.setter
+    def features(self, features):
+        """ . """
+        self._features = features
+        self._draw_patches()
+        return
+
+    @property
+    def offset(self):
+        return self._offset
+
+    @offset.setter
+    def offset(self, offset):
+        self._offset = offset
+        self._draw_patches()
+        return
+
+    @property
+    def by_axis(self):
+        return self._by_axis
+
+    @by_axis.setter
+    def by_axis(self, by_axis):
+        self._by_axis = by_axis
+        if by_axis is not None:
+            for patch in self.patches:
+                if patch.by_axis is not None:
+                    patch.by_axis = by_axis
+        return
+
+    @property
+    def paths(self):
+        return self._paths
+
+    @paths.setter
+    def paths(self, paths):
+        self._paths = paths
+        return
+
+    def get_paths(self):
+        """ alias for paths property """
+        return self.paths
+
+    def set_paths(self, paths):
+        """ alias for paths.setter """
+        self.paths = paths
+        return
+
+    def _set_paths(self):
+        self.paths = [
+            p.get_transform().transform_path(p.get_path())
+            for p in self._patches
+            ]
+        return
+
+    def _set_props(self):
+
+        def determine_facecolor(patch):
+            if patch.get_fill():
+                return patch.get_facecolor()
+            return [0, 0, 0, 0]
+
+        props = defaultdict(list)
+        valid = {
+            "facecolors": (self.set_facecolor, determine_facecolor),
+            "edgecolors": (self.set_edgecolor, Patch.get_edgecolor),
+            "linewidths": (self.set_linewidth, Patch.get_linewidth),
+            "linestyles": (self.set_linestyle, Patch.get_linestyle),
+            "antialiaseds": (self.set_antialiased, Patch.get_antialiased),
+            }
+
+        for p in self._patches:
+            for key, (set_, get) in valid.items():
+                props[key].append(get(p))
+
+        for key, (set_, get) in valid.items():
+            set_(props[key])
+        return
+
+    def _draw_patches(self):
         """ . """
         patches = list()
-        for f in self.features:
-            patches.extend(f.draw())
+        for feature in self.features:
+            if self._offset != 0:
+                feature.offset = self._offset
+            if isinstance(feature, Patch):
+                patches.append(feature)
+            elif isinstance(feature, Feature):
+                patches.extend(feature.patches)
+            elif isinstance(feature, FeatureGroup):
+                patches.extend(feature.patches)
+            else:
+                pass
 
-        if ax is not None:
-            for patch in patches:
-                ax.add_patch(patch)
-
-        return patches
-
-    def add_bpfeatures(
-            self,
-            features,
-            obj=Feature
-            ):
-        ''' . '''
-        new_features = list()
-        for feature in features:
-            blocks = list()
-            reverse = feature.strand == -1
-            parts = sorted(
-                feature.location.parts,
-                key=lambda f: min(f.start, f.end),
-                reverse=reverse
-                )
-            for part in parts:
-                #  Add blocks as [start, end].
-                block = [
-                    int(min(part.start, part.end)),
-                    int(max(part.start, part.end))
-                    ][::feature.strand]
-                blocks.append(block)
-
-            o = obj()
-            o.add(blocks)
-            o.name = feature.id
-            new_features.append(o)
-
-        self.features.extend(new_features)
-        return new_features
+        self._patches = patches
+        return
 
 
 class LinkCollection(object):
